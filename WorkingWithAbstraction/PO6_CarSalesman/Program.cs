0f31a22f8a065de5b8cd7863689eae05aa860ee1
PetrovPetar5 <74185222+PetrovPetar5@users.noreply.github.com>
2021-09-03 17:18:20 +0300

    Add files via upload

---
+++ WorkingWithAbstraction/PO6_CarSalesman/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PO6_CarSalesman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WorkingWithAbstraction/PO7_JediGalaxy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PO7_JediGalaxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
